5ca11affb899a6813eb80cd80e5ab3da05c4b884
DavidM <davidmauricio.gutierrezacuna@gmail.com>
2026-03-10 10:29:36 -0500

    Jumpad nuevo

--- Assets/Scripts/Personaje/Jumpad.cs
+++ Assets/Scripts/Personaje/Jumpad.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Jumpad : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
             {
                 Vector2 jumpVector = new Vector2(0, 1);
-                rb.AddForce(jumpVector * 150, ForceMode2D.Impulse);
+                rb.AddForce(jumpVector * 180, ForceMode2D.Impulse);
                 Debug.Log("Jump Pad Activated");
             }
         }
