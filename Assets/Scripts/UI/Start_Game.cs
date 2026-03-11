d54414719809b9f59ac66a1376949e13d1385fbe
Yamid-E <127436285+Yamid-E@users.noreply.github.com>
2026-03-11 09:26:32 -0500

    Ui

--- Assets/Scripts/UI/Start_Game.cs
+++ Assets/Scripts/UI/Start_Game.cs
@@ -1,28 +1,18 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // <--- NUEVO: Necesario para reiniciar niveles
+using UnityEngine.SceneManagement; // <--- Línea obligatoria para cambiar de escena
 
 public class Start_Game : MonoBehaviour
 {
-    public GameObject pantallaMenu;     
-    public GameObject barraEnergia;
-    public GameObject pantallaGameOver; // <--- NUEVO: Para poder apagarla si es necesario
+    [Header("Configuración de Escenas")]
+    public string nombreNivel1 = "Level_01"; 
 
     public void IniciarJuego()
     {
-        // Apagamos el menú
-        pantallaMenu.SetActive(false);
-        
-        // Encendemos la interfaz del juego
-        barraEnergia.SetActive(true);
-        
-        // Debug.Log("¡El juego ha empezado!");
+        SceneManager.LoadScene(nombreNivel1);
     }
 
-    // --- ESTA ES LA FUNCIÓN PARA EL BOTÓN DE RETRY ---
     public void ReiniciarNivel()
     {
-        // Esta línea busca el nombre de la escena actual y la vuelve a cargar
-        // Es la forma más limpia de resetear robots, enemigos y energía
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
